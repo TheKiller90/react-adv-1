ea763a98d8cd6da7cdf9ae17df08e795fc0d8aac
HazelP <antonio.pimentel.hazel@gmail.com>
2022-12-01 18:11:19 -0600

    Lazyload y suspense agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { NavLink, Route, Switch, BrowserRouter as Router, Redirect } from 'react-router-dom';
 
 import logo from '../logo.svg';
@@ -5,39 +6,41 @@ import { routes } from './routes';
 
 export const Navigation = () => {
 	return (
-		<Router>
-			<div className="main-layout">
-				<nav>
-					<img src={logo} alt="React logo" />
+		<Suspense fallback={ <span>Loading...</span> }>
+			<Router>
+				<div className="main-layout">
+					<nav>
+						<img src={logo} alt="React logo" />
 
-					<ul>
+						<ul>
+							{
+								routes.map(({ path, name }) => (
+									<li key={ path }>
+										<NavLink
+											to={ path }
+											activeClassName="nav-active"
+											exact
+										>{ name }</NavLink>
+									</li>
+								))
+							}
+						</ul>
+					</nav>
+
+					<Switch>
 						{
-							routes.map(({ path, name }) => (
-								<li key={ path }>
-									<NavLink
-										to={ path }
-										activeClassName="nav-active"
-										exact
-									>{ name }</NavLink>
-								</li>
+							routes.map(({ component:Component, path }) => (
+								<Route
+									key={ path }
+									path={ path }
+									render={ () => <Component /> }
+								/>
 							))
 						}
-					</ul>
-				</nav>
-
-				<Switch>
-					{
-						routes.map(({ component:Component, path }) => (
-							<Route
-								key={ path }
-								path={ path }
-								render={ () => <Component /> }
-							/>
-						))
-					}
-					<Redirect to={ routes[0].path } />
-				</Switch>
-			</div>
-		</Router>
+						<Redirect to={ routes[0].path } />
+					</Switch>
+				</div>
+			</Router>
+		</Suspense>
 	);
 }
