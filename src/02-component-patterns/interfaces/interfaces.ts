9e7c566c0cb96e63309cfda97b8f699b0a59c915
HazelP <antonio.pimentel.hazel@gmail.com>
2022-12-04 00:52:19 -0600

    Control props implementado

--- src/02-component-patterns/interfaces/interfaces.ts
+++ src/02-component-patterns/interfaces/interfaces.ts
@@ -4,20 +4,29 @@ import { Props as ProductImageProps } from "../components/ProductImage";
 import { Props as ProductTitleProps } from "../components/ProductTitle";
 
 export interface Product {
-    id: string,
-    img?: string,
-    title: string
+	id: string,
+	img?: string,
+	title: string
 };
 
 export interface ProductContextProps {
-  counter: number,
-  increaseBy: (value:number) => void,
-  product: Product
+	counter: number,
+	increaseBy: (value: number) => void,
+	product: Product
 };
 
 export interface ProductCardHOCProps {
-  ({ children, product }: ProductCardProps):JSX.Element,
-  Buttons: (Props:ProductButtonsProps) => JSX.Element,
-  Image: (Props:ProductImageProps) => JSX.Element,
-  Title: (Props:ProductTitleProps) => JSX.Element
+	({ children, product }: ProductCardProps): JSX.Element,
+	Buttons: (Props: ProductButtonsProps) => JSX.Element,
+	Image: (Props: ProductImageProps) => JSX.Element,
+	Title: (Props: ProductTitleProps) => JSX.Element
+};
+
+export interface OnChangeArgs {
+	product: Product,
+	count: number
+}
+
+export interface ProductInCart extends Product {
+	count: number
 };
